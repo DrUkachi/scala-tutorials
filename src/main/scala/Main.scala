edec2d63e4f39be75dc72620047b96258e5dd175
Ukachi Osisiogu <ukachi.osi@gmail.com>
2025-03-01 08:26:08 +0000

    Columns

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,7 @@
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions.col
+
 
 object Main { 
   def main(args: Array[String]): Unit = {
@@ -7,12 +10,22 @@ object Main {
       .master("local[*]")
       .getOrCreate()
 
-    val df = spark.read
+    val df: DataFrame = spark.read
       .option("header", "true")
+      .option("inferSchema", "true")
       .csv("/teamspace/studios/this_studio/scala-work/data/AAPL.csv")
-
+      
     df.printSchema()
-    df.show()
-    spark.stop()
+    df.show(10)
+
+    import spark.implicits._
+    println("Selecting columns using select method")
+    df.select("Date", "Open", "Close").show(10)
+    println("Selecting columns using col method")
+    df.select(col("Date"), col("Open"), col("Close")).show(10)
+    println("Selecting columns using $ method")
+    df.select($"Date", $"Open", $"Close").show(10)
+    println("Selecting columns using $ method with select method")
+    df.select(col("Date"), $"Open", df("Close")).show(10)
   }
 }
